ddedd302db26e0fab2923dcf10d56c3446c3efe7
Mad At <bhrantiknagar@gmail.com>
2026-06-24 15:10:11 +0530

    day_13.5

---
+++ modules.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
