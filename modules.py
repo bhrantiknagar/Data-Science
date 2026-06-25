7d1ea2fdf00aa68ffe9cac7b6045f1e136d8a2f9
Mad At <bhrantiknagar@gmail.com>
2026-06-25 14:53:32 +0530

    day_14 Confusion matrix

--- modules.py
+++ modules.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
